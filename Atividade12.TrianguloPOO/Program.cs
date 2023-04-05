c006f7e4590a8846cf7414ce61ac5251a90989ac
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-04-05 17:16:21 -0300

    Add project files.

---
+++ Atividade12.TrianguloPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Atividade12.TrianguloPOO
+{
+     internal class Program
+     {
+          static void Main(string[] args)
+          {
+               do
+               {
+                    Triangulo triangulo = new Triangulo();
+                    Console.Clear();
+                    triangulo.x = triangulo.PegarValor("Entre com o comprimento de X:\n> ");
+                    triangulo.y = triangulo.PegarValor("\nEntre com o comprimento de Y:\n> ");
+                    triangulo.z = triangulo.PegarValor("\nEntre com o comprimento de Z:\n> ");
+                    triangulo.GerarTriangulo();
+
+               } while(true);
+          }
+     }
+}
---
+++ Atividade12.TrianguloPOO/Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade12.TrianguloPOO
+{
+     internal class Triangulo
+     {
+          public decimal x, y, z;
+
+          public decimal PegarValor(string mensagem)
+          {
+               Console.Write(mensagem);
+               decimal valor = Convert.ToDecimal(Console.ReadLine());
+               return valor;
+          }
+          public void GerarTriangulo()
+          {
+               if ((x + y < z) || (x + z < y) || (y + z < x))
+               {
+                    Console.WriteLine("\nTriângulo não pode ser criado!");
+                    Console.ReadLine();
+               }
+               else if ((x == y) && (y == z))
+               {
+                    Console.WriteLine("\nTriângulo equilátero criado!");
+                    Console.ReadLine();
+               }
+               else if ((x == z) || (z == y) || (x == y))
+               {
+                    Console.WriteLine("\nTriângulo isósceles criado!");
+                    Console.ReadLine();
+               }
+               else
+               {
+                    Console.WriteLine("\nTriângulo escaleno criado!");
+                    Console.ReadLine();
+               }                          
+          }
+     }
+}
